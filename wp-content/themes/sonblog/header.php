8e4f43ed56e483e5e00a5b86826fd4dafe27b532
DanielCabral <Daniel-cabral14@hotmail.com>
2020-05-23 19:20:13 -0300

    Mostrando posts e paginas no menu

--- wp-content/themes/sonblog/header.php
+++ wp-content/themes/sonblog/header.php
@@ -2,10 +2,32 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Blog da School of Net</title>
+    <title><?php bloginfo("name");?></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 </head>
 <body>
 <header>
-    <h1>Header do Blog</h1>
+    <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+            <a class="navbar-brand" href="<?php bloginfo("url");?>"><?php bloginfo("name");?></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav">
+                        <?php
+                            $pages=get_pages();
+                            foreach ($pages as $page):
+                            $link=get_page_link($page->ID);
+                            $title=$page->post_title;
+                        ?>
+                     <li>
+                        <a href="<?php $link?>"><?php echo $title;?></a>
+                    </li>
+                        <?php
+                            endforeach;
+                        ?>
+                </ul>
+            </div>
+    </nav>
 </header>
--- wp-content/themes/sonblog/index.php
+++ wp-content/themes/sonblog/index.php
@@ -2,16 +2,36 @@
 <?php get_header("personalizado");?>
 <div class="container">
     <div class="row">
-        <div class="col-md-6">
-            <p>
-                Blog da school of net
-            </p>
-        </div>
-        <div class="col-md-3">
-            <?php get_sidebar();?>
-        </div>
-        <div class="col-md-3">
-            <?php get_sidebar("personalizado");?>
+        <div class="col-md-2">
+        <div class="col-md-10 ">
+            <div class="shadow-sm p-3 mb-5 bg-white rounded">
+                <p>
+                    <?php
+
+
+                    if (have_posts() ) :
+                        while(have_posts()): the_post();
+                            ?>
+                                <div class="card" style="width: 18rem;">
+                                    <?php
+                                        if(has_post_thumbnail()):
+                                    ?>
+                                        <img class="card-img-top" src="<?php echo  get_the_post_thumbnail();?>">
+                                    <?php endif;?>
+                                    <div class="card-body">
+                                        <h5 class="card-title"><?php echo get_the_title();?></h5>
+                                        <p class="card-text"><?php echo $post->post_content;?></p>
+                                        <a href="<?php echo get_the_permalink()?>" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                        <?php
+                            endwhile;
+                            else:
+                                echo "<p>Não há noticias</p>";
+                        ?>
+                    <?php endif; ?>
+                </p>
+                <div>
         </div>
     </div>
 </div>
